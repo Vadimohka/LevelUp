72fe5b9290af2af05721f65b9809f1f91f68aa74
Nikolssen <56168159+Nikolssen@users.noreply.github.com>
2019-10-12 18:50:03 +0300

    Added Task3 and Task4

---
+++ Budovich/HT2/Task3/task3.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <cctype>
+using namespace std;
+
+int fact(int x) {
+
+	int res;
+
+	res = 1;
+	for (int j = 1; j <= x; j++) {
+		res = res * j;
+	}
+	return res;
+}
+int main()
+{
+	char word[8];
+	char sieg, changer;
+	int i = 0;
+	int j = 0;
+	int wordsize, wordsize2;
+	int repeatletters[7]{ 1,1,1,1,1,1,1 };
+    cout << "Enter word using only latin letters. Print 0 to finish before.\n";
+	while (i<7) {
+		cin>>sieg;
+		if (isalpha(sieg)) {
+			word[i] = sieg;
+			i++;
+		}
+		if (sieg == '0')
+		{
+			wordsize = i;
+			break;
+		}
+		else wordsize = 7;
+		}
+	wordsize2 = wordsize;
+	
+
+
+
+    i = 0;
+	while(i<wordsize2){
+		j = i+1;
+		while (j < wordsize2) {
+			if (word[i] == word[j]) {
+				repeatletters[i]++;
+				changer = word[wordsize2-1];
+				word[wordsize2 -1] = word[j];
+				word[j] = changer;
+				wordsize2--;
+			}
+			else j++;
+		}i++;
+		}
+
+		i = fact(wordsize);
+		for (int k = 0; k < wordsize; k++)
+			i /= fact(repeatletters[k]);
+		cout << "Amount of annograms is " << i;
+
+	}
+
+
---
+++ Budovich/HT2/Task4/Task4.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <ctype.h>
+
+using namespace std;
+int main()
+{
+	char str[81];
+	int maxlength[40];
+	int i = 0;
+	int k = 0;
+	int max = 0;
+	int max1 = 0;
+	gets_s(str);
+	for (int j = 0;j < 40;j++) maxlength[j] = 0;
+	
+	while (str[i] != '\0') //Преобразовывает все небуквы в пробелы
+	{
+		if (!isalpha(str[i])) 
+		{
+			str[i] = ' ';
+			k++;
+     	}
+		else 
+			maxlength[k]++;
+		if (maxlength[k] > max1) { max1 = maxlength[k]; max = k; }
+
+		i++;
+	}
+	
+		if (max == 0) {
+			for (int k = 0;k <= max1;k++) 
+			cout << str[k];
+		}
+		else
+		{
+			int h = 0;
+			for (int k = 0; k <= i;k++) {
+				if (str[k] == ' ') h++;
+				if (h == max) {
+					cout << str[k];
+				}
+			}
+		}
+		cout << " is the first longest.";
+	
+}
+
