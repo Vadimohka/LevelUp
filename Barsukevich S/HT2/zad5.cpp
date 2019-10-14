83693a6e7064d1cb4e1dd3dc4545e7d62ae66da9
Ex7ARZ <mx2002bsn@gmail.com>
2019-10-15 00:19:05 +0300

    Create zad5.cpp

---
+++ Barsukevich S/HT2/zad5.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	 int a, b, c,a1,b1;
+	int k = 0;
+	cout << "delimoe ";
+	cin >> a1;
+	cout << "delitel ";
+	cin >> b1;
+	if (b1 == 0) { cout << "wrong value b,iput b again,b="; cin >> b1; }
+	a = abs(a1);
+	b = abs(b1);
+	if (a > b) {
+		do {
+			a -= b;
+			k++;
+		} while (a > b);
+		if (a > b) { k++; c = 0; }
+		else c = a;
+	}
+	else if(a>0) {
+		k = 0; c = a;
+	}
+	else { k = 0; c = 0; }
+	if (a1 > 0 && b1 < 0) k = -1 * k;
+	if (a1 < 0 && b1 > 0) { k *= -1; c *= -1; }
+	
+cout << "chastnoe "<< k<<"  ostatok " << c;
+		
+
+	return 0;
+}
